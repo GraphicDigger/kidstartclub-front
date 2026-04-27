bef11048a0dee4877267ebc81271a72d3de8164e
GraphicDigger <ilmirdesign@gmail.com>
2026-04-27 22:51:09 +0300

    dfadfadf

--- src/entities/course/model/hooks/useCourseMutation.ts
+++ src/entities/course/model/hooks/useCourseMutation.ts
@@ -1,22 +1,9 @@
 import { Course } from '../../types';
-import { useRepository } from '@/shared/services/dataAccess';
-import { createCourse } from '../store/factory';
 
 export const useCourseMutation = () => {
-  const repo = useRepository();
-
-  const addCourse = (input: Partial<Course>) => {
-    const course = createCourse(input);
-    repo.courses.addCourse(course);
-  };
-
-  const updateCourse = (course: Course) => {
-    repo.courses.updateCourse(course);
-  };
-
-  const deleteCourse = (courseId: Course['id']) => {
-    repo.courses.removeCourse(courseId);
-  };
+  const addCourse = (_input: Partial<Course>) => {};
+  const updateCourse = (_course: Course) => {};
+  const deleteCourse = (_courseId: Course['id']) => {};
 
   return {
     addCourse,
